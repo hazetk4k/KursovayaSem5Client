ef150eec707253beb176ec8513023c70fc601aa0
aleksei <mumiayr80@gmail.com>
2022-12-07 19:52:49 +0300

    add: add quality stuff

--- src/main/java/client/ProductQualityController.java
+++ src/main/java/client/ProductQualityController.java
@@ -18,6 +18,7 @@ public class ProductQualityController extends BaseController implements Initiali
     public Button btnSaveResult;
     public Button btnGraph;
 
+    public String[] parts = {"Топливная система\t", "Аккумулятор\t", "Кузов\t", "Колеса\t"};
     public void onBtnBack(ActionEvent actionEvent) {
         nextWindow("MainFrame", btnBack, "Главное окно");
     }
@@ -27,6 +28,18 @@ public class ProductQualityController extends BaseController implements Initiali
     }
 
     public void onBtnGraph(ActionEvent actionEvent) {
+    }
+
+    public String getFormula(String detail, String base, String result) {
+        String formula = "";
+        String[] details = detail.split("; ");
+        String[] bases = detail.split("; ");
+        String[] results = detail.split("; ");
+        for (int i = 1; i < 4; i++) {
+            formula += (details[i] + " / " + bases[i] + " = " + results[i-1]);
+            formula += "///";
+        }
+        return formula;
     }
 
     @Override
@@ -40,20 +53,21 @@ public class ProductQualityController extends BaseController implements Initiali
             out.println("Отчет" + "; " + BaseController.chosenID);
             line = in.readLine();
             String[] lines = line.split("///");
+            // 1) продукт ///2) фуел ///3) бэт ///4) кар ///5) колеса ///6) базовые1 ///7) базовые2 ///8) базовые3 ///9) базовые4 ///10) кач1 ///11) кач2 ///12) кач3 ///13) кач4 ///14) результат1 ///15) рузультат 2 ///16) рез3 ///17) рез4 ///
             listResults.getItems().add("Параматры продукта: " + lines[0]);
-            listResults.getItems().add("Параметры топливной системы: " + lines[1]);
-            listResults.getItems().add("Параметры аккумулятора: " + lines[2]);
-            listResults.getItems().add("Параметры кузова: " + lines[3]);
-            listResults.getItems().add("Параметры колес: " + lines[4]);
+            listResults.getItems().add("Параметры топливной системы: \t" + lines[1]);
+            listResults.getItems().add("Параметры аккумулятора: \t" + lines[2]);
+            listResults.getItems().add("Параметры кузова: \t" + lines[3]);
+            listResults.getItems().add("Параметры колес: \t" + lines[4]);
             listResults.getItems().add("Дифференциальныый метод расчета уровня качества: ");
-            listResults.getItems().add("BaseFuel: " + lines[5]);
-            listResults.getItems().add("BaseBattery: " + lines[6]);
-            listResults.getItems().add("BaseCarcase: " + lines[7]);
-            listResults.getItems().add("BaseWheels: " + lines[8]);
-            listResults.getItems().add("FuelRes: " + lines[9]);
-            listResults.getItems().add("BatteryRes: " + lines[10]);
-            listResults.getItems().add("CarcaseRes: " + lines[11]);
-            listResults.getItems().add("WheelsRes: " + lines[12]);
+            for (int i = 1; i < 5; i++) {
+                listResults.getItems().add(parts[i - 1]);
+                String[] formulas = getFormula(lines[i], lines[i + 5], lines[i + 9]).split("///");
+                for(int j=0; j <3; j++){
+                    listResults.getItems().add(formulas[j]);
+                }
+                listResults.getItems().add(lines[i + 13]);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
